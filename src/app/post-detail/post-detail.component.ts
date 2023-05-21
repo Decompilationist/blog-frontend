3a1a637e43fd8579696fe063103d19888d42a531
Gustavo Menezes <103705903+Decompilationist@users.noreply.github.com>
2023-05-21 20:45:12 -0300

    Privatizando rotas e função goBack()

--- src/app/post-detail/post-detail.component.ts
+++ src/app/post-detail/post-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { BlogService } from '../blog.service';
 
 @Component({
@@ -10,15 +11,20 @@ import { BlogService } from '../blog.service';
 export class PostDetailComponent implements OnInit {
   post: any;
 
-  constructor(private route: ActivatedRoute, private blogService: BlogService) { }
+  constructor(private route: ActivatedRoute, private blogService: BlogService, private router: Router) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       const postId = params.get('id') ?? ''; // Definindo uma string vazia como valor padrão
       this.blogService.getPostById(postId).subscribe(post => {
         this.post = post;
+        console.log(this.post); // verificar se os dados do post são exibidos de forma correta no console
       });
     });
     
   }
+  goBack(): void {
+    this.router.navigate(['/']);
+  }
 }
+
