1d1be01df55352866efb93c0735f08188bcedd06
Gustavo Menezes <103705903+Decompilationist@users.noreply.github.com>
2023-05-21 21:17:38 -0300

    estilizando a home e o post details

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    purge: [],
-    darkMode: false,
-    theme: {
-      extend: {},
-    },
-    variants: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
